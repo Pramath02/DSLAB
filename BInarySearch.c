0fc947e2f73d4286359f3ec1462c650abe211def
Pramath02 <pramath49babaji@gmail.com>
2023-01-06 23:48:32 +0530

    second commit second commit :wq

---
+++ BInarySearch.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int binarysearch(int array[],int x,int low, int high)
+{
+ if(high>=low)
+ {
+  int mid=low+(high-low)/2;
+  
+  if(array[mid]==x)
+  {
+   return mid;
+  }
+   if(array[mid]>x)
+   {
+    return binarysearch(array,x,low,mid-1);
+   }
+    else
+    {
+     return binarysearch(array,x,mid+1,high);
+    }
+  return-1;
+ }
+}
+
+int main(void)
+{
+ int array[]={3,4,5,6,7,8,9};
+ int n= sizeof(array)/sizeof(array[0]);
+ int x=6;
+ int result=binarysearch(array,x,0,n-1);
+ if(result==-1)
+ {
+ printf("element not found");
+ } 
+ else
+ {
+ printf("Element is found at index: %d",result);
+ }
+ }
